87d4541d98bbdc57bdc06332457b30771b85c464
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2015-05-04 14:45:52 -0300

    Jogo funcionando.

--- BreakoutSpriteKitTutorial/BreakoutSpriteKitTutorial/GameScene.swift
+++ BreakoutSpriteKitTutorial/BreakoutSpriteKitTutorial/GameScene.swift
@@ -13,20 +13,65 @@ let PaddleCategoryName = "paddle"
 let BlockCategoryName = "block"
 let BlockNodeCategoryName = "blockNode"
 
-class GameScene: SKScene {
+let BallCategory   : UInt32 = 0x1 << 0
+let BottomCategory : UInt32 = 0x1 << 1
+let BlockCategory  : UInt32 = 0x1 << 2
+let PaddleCategory : UInt32 = 0x1 << 3
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var isFingerOnPaddle = false
     
     override func didMoveToView(view: SKView) {
         super.didMoveToView(view)
-        
+
         let borderBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
         borderBody.friction = 0
         self.physicsBody = borderBody
+        
         physicsWorld.gravity = CGVectorMake(0, 0)
         
         let ball = childNodeWithName(BallCategoryName) as! SKSpriteNode
-        ball.physicsBody!.applyImpulse(CGVectorMake(10, -10))
+        ball.physicsBody!.applyImpulse(CGVectorMake(5, -15))
+        
+        let bottomRect = CGRectMake(frame.origin.x, frame.origin.y, frame.size.width, 1)
+        let bottom = SKNode()
+        bottom.physicsBody = SKPhysicsBody(edgeLoopFromRect: bottomRect)
+        addChild(bottom)
+        
+        let paddle = childNodeWithName(PaddleCategoryName) as! SKSpriteNode
+        
+        bottom.physicsBody!.categoryBitMask = BottomCategory
+        ball.physicsBody!.categoryBitMask = BallCategory
+        paddle.physicsBody!.categoryBitMask = PaddleCategory
+        
+        ball.physicsBody!.contactTestBitMask = BottomCategory | BlockCategory
+        
+        physicsWorld.contactDelegate = self
+
+        let numberOfBlocks = 5
+        
+        let blockWidth = SKSpriteNode(imageNamed: "block.png").size.width
+        let totalBlocksWidth = blockWidth * CGFloat(numberOfBlocks)
+        
+        let padding: CGFloat = 10.0
+        let totalPadding = padding * CGFloat(numberOfBlocks - 1)
+        
+        let xOffset = (CGRectGetWidth(frame) - totalBlocksWidth - totalPadding) / 2
+        
+        for i in 0..<numberOfBlocks {
+            let block = SKSpriteNode(imageNamed: "block.png")
+            block.position = CGPointMake(xOffset + CGFloat(CGFloat(i) + 0.5)*blockWidth + CGFloat(i-1)*padding, CGRectGetHeight(frame) * 0.8)
+            block.physicsBody = SKPhysicsBody(rectangleOfSize: block.frame.size)
+            block.physicsBody!.allowsRotation = false
+            block.physicsBody!.friction = 0.0
+            block.physicsBody!.dynamic = false
+            block.physicsBody!.affectedByGravity = false
+            block.name = BlockCategoryName
+            block.physicsBody!.categoryBitMask = BlockCategory
+            addChild(block)
+        }
+        
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
@@ -62,6 +107,61 @@ class GameScene: SKScene {
     
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
         isFingerOnPaddle = false
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if firstBody.categoryBitMask == BallCategory && secondBody.categoryBitMask == BottomCategory {
+            if let mainView = view {
+                let gameOverScene = GameOverScene.unarchiveFromFile("GameOverScene") as! GameOverScene
+                gameOverScene.gameWon = false
+                mainView.presentScene(gameOverScene)
+            }
+        }
+        
+        if firstBody.categoryBitMask == BallCategory && secondBody.categoryBitMask == BlockCategory {
+            secondBody.node!.removeFromParent()
+            if isGameWon() {
+                if let mainView = view {
+                    let gameOverScene = GameOverScene.unarchiveFromFile("GameOverScene") as! GameOverScene
+                    gameOverScene.gameWon = true
+                    mainView.presentScene(gameOverScene)
+                }
+            }
+        }
+    }
+    func isGameWon() -> Bool {
+        var numberOfBricks = 0
+        self.enumerateChildNodesWithName(BlockCategoryName) {
+            node, stop in
+            numberOfBricks = numberOfBricks + 1
+        }
+        return numberOfBricks == 0
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        let ball = self.childNodeWithName(BallCategoryName) as! SKSpriteNode
+        
+        let maxSpeed: CGFloat = 1000.0
+        let speed = sqrt(ball.physicsBody!.velocity.dx * ball.physicsBody!.velocity.dx + ball.physicsBody!.velocity.dy * ball.physicsBody!.velocity.dy)
+        
+        if speed > maxSpeed {
+            ball.physicsBody!.linearDamping = 0.4
+        }
+        else {
+            ball.physicsBody!.linearDamping = 0.0
+        }
     }
     
 }
